16358702a576e827af5e7e7bba3fed32502cb5a5
William Kennedy <wkenney@protonmail.com>
2024-03-17 14:35:36 -0400

    New part of the app for reading financial statements

---
+++ statements.py
@@ -0,0 +1,9 @@
+"""
+Author: William Kennedy
+
+"""
+import yfinance as yf
+import pandas as pd
+import dash
+from dash import dcc, html, callback, Input, Output, State
+import dash_bootstrap_components as dbc
